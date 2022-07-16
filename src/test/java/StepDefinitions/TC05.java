ec4f2b15952006ec33248a19035e73819714eac3
Zeynep Kaya <zeynepkaya@Zeyneps-MacBook-Pro.local>
2022-07-16 10:23:41 +0200

    16-07-2022

---
+++ src/test/java/StepDefinitions/TC05.java
@@ -0,0 +1,33 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class TC05 {
+    @Given("Navigate to url")
+public void navigate_to_url() {
+
+
+}
+    @Then("Click on {string} button")
+    public void click_on_button(String string) {
+
+    }
+    @Then("Verify {string} is visible")
+    public void verify_is_visible(String string) {
+
+    }
+    @Then("Enter name and already registered email address")
+    public void enter_name_and_already_registered_email_address() {
+
+    }
+    @Then("Click {string} button")
+    public void click_button(String string) {
+
+    }
+    @Then("Verify error {string} is visible")
+    public void verify_error_is_visible(String string) {
+
+
+
+    }}

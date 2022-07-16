0e60cbb38163a5cdf5cdd8a84fa66b0e48049d99
Abdulsamet Akbaba <bilinmezlerdenklemi@gmail.com>
2022-07-16 16:46:02 +0100

    16.07.2022

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -13,8 +13,8 @@ import org.junit.runner.RunWith;
                 "rerun:target/failedRerun.txt"
         },
         features = "./src/test/resources/features",
-        glue = "stepdefinitions",
-        tags = "@negativeLogin",
+        glue = "StepDefinitions",
+        tags = "@au",
         dryRun = false
 )
 public class TestRunner {

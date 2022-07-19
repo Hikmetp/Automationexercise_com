1bba2e8c0a3c824f306d7efcdd1b4c6d465a0b0d
Mine Yilmaz <mineakyildiz@gmail.com>
2022-07-19 10:40:39 +0100

    19.7.2022

---
+++ src/test/java/StepDefinitions/TC10_StepDefinition.java
@@ -0,0 +1,21 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.Then;
+
+public class TC10_StepDefinition {
+    @Then("Scroll down to footer")
+    public void scrollDownToFooter() {
+    }
+
+    @Then("Verify text SUBSCRIPTION")
+    public void verifyTextSUBSCRIPTION() {
+    }
+
+    @Then("Enter email address in input and click arrow button")
+    public void enterEmailAddressInInputAndClickArrowButton() {
+    }
+
+    @Then("Verify success message You have been successfully subscribed! is visible")
+    public void verifySuccessMessageYouHaveBeenSuccessfullySubscribedIsVisible() {
+    }
+}
